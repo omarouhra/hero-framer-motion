2395969e9bf0178c4eb9b27a84f50ee5f68dbefa
omarouhra <omarmaroc26@gmail.com>
2022-08-11 15:59:17 +0100

    starter branch 🚀

--- src/components/AnimatedLetters.tsx
+++ src/components/AnimatedLetters.tsx
@@ -1,46 +1,21 @@
-import React from 'react'
-import { motion } from 'framer-motion'
-import { Animations } from '../variants/animations';
-
-
+import React from "react";
 
 type letterType = {
     title: string;
-}
+};
 
 function AnimatedLetters({ title }: letterType) {
-
     // splite words
-    const splitWords = title.split(" ");
-
 
     // splite words to letters
-    const words = splitWords.map(word => {
-        return word.split('')
-    })
-
 
     //  adding spacing
-    words.map(word => {
-        return word.push("\u00A0")
-    })
-
-
 
     return (
-        <div className="overflow-hidden bg-white ">
-
-            { words.flat().map((element, index) => {
-                return (
-                    <motion.span
-                        className='text-3xl md:text-7xl font-cal mt-3 text-white mix-blend-difference transition duration-500 '
-                        style={ { display: "inline-block" } }
-                        variants={ Animations.letter }
-                        key={ index }>{ element }</motion.span>
-                )
-            }) }
+        <div className='overflow-hidden bg-white '>
+            <span className='text-3xl md:text-7xl font-cal mt-3 text-white mix-blend-difference transition duration-500'>{ title }</span>
         </div>
-    )
+    );
 }
 
-export default AnimatedLetters
+export default AnimatedLetters;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,96 +1,70 @@
-import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import AnimatedLetters from "../components/AnimatedLetters";
-import { Animations } from '../variants/animations';
-
-
 
 
 const Home: NextPage = () => {
-
   // UseState variables
-  const [showContent, setShowContent] = useState<boolean>(false)
-  const [showImage, setShowImage] = useState<boolean>(false)
-
+  const [showContent, setShowContent] = useState<boolean>(false);
+  const [showImage, setShowImage] = useState<boolean>(false);
 
   return (
     <div>
       {/* Splash Screen */ }
-      <motion.div
-        initial={ {
-          height: "100%",
-        } }
-        animate={ {
-          height: "0%",
-          transition: { ease: [0.455, 0.03, 0.515, 0.955], duration: 2.9 }
-        } }
-        onAnimationComplete={ () => setShowImage(true) }
-        className="absolute bg-black h-screen w-full ">
-      </motion.div>
+      <div className='absolute bg-black h-screen w-full '></div>
 
-      <div className="mx-auto max-w-6xl py-4 md:py-12 px-5 ">
-        
+      <div className='mx-auto max-w-6xl py-4 md:py-12 px-5 '>
         <Head>
           <title>Omar Portfolio</title>
-          <meta name="description" content="Portfolio animation with framer motion 🚀" />
-          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name='description'
+            content='Portfolio animation with framer motion 🚀'
+          />
+          <link rel='icon' href='/favicon.ico' />
         </Head>
 
-        <main className="py-2 md:py-12 select-none">
-          <motion.section
-            initial="hidden"
-            animate="visible"
-            variants={ Animations.container}
+        <main className='py-2 md:py-12 select-none'>
+          <section>
+            {/* Picture */ }
+            <div className='relative w-[100%] md:w-[70%]'>
+              <Image
+                src='https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+                alt='image-1'
+                layout='fill'
+                className='rounded-md object-cover'
+              />
+            </div>
 
-          >
+            <div className='py-8  '>
+              <div className='text-2xl animate-wiggle w-min '>👋</div>
 
-            {/* Picture */}
-            { showImage && <motion.div
-              variants={ Animations.picture }
-              onAnimationComplete={ () => setShowContent(true) }
-              className="relative w-[100%] md:w-[70%]">
-              <Image src='https://images.unsplash.com/photo-1583508915901-b5f84c1dcde1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80' alt='image-1' layout='fill' className="rounded-md object-cover" />
-            </motion.div> }
-
-            <div className="py-8  ">
-              <motion.div variants={ Animations.FadeRight } className="text-2xl animate-wiggle w-min ">👋</motion.div>
-
-              {/* Animated Letters */}
+              {/* Animated Letters */ }
               <AnimatedLetters title="Hi, I'm Omar" />
 
-              {/* Content */}
-              { showContent &&
+              {/* Content */ }
+              <div>
+                <p className='text-gray-400 text-base font-light md:w-[70%] my-3'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Itaque esse excepturi incidunt sequi quaerat asperiores?
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Itaque esse excepturi incidunt sequi quaerat asperiores?
+                </p>
+
                 <div>
-                  <motion.p
-                    variants={ Animations.FadeLeft }
-                    className="text-gray-400 text-base font-light md:w-[70%] my-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse excepturi incidunt sequi quaerat asperiores? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse excepturi incidunt sequi quaerat asperiores?</motion.p>
-
-                  <motion.div
-                    variants={ Animations.FadeRight }
-                  >
-                    <Link href='' >
-                      < a className="hover:underline" >
-                        See more →
-                      </ a>
-                    </Link >
-                  </motion.div>
+                  <Link href=''>
+                    <a className='hover:underline'>See more →</a>
+                  </Link>
                 </div>
-              }
+              </div>
             </div>
-          </motion.section>
-        </main >
-      </div >
-    </div >
-
+          </section>
+        </main>
+      </div>
+    </div>
   );
 };
-
-
-
-
 
 export default Home;
