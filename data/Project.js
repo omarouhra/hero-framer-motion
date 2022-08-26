da18302de5ae1f574ab720d1123dc2ae95b7ee8f
omarouhra <omarmaroc26@gmail.com>
2022-08-26 17:04:51 +0100

    feat: starter project

---
+++ data/Project.js
@@ -0,0 +1,16 @@
+const PROJECTS = [
+  {
+    name: "project 1",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse excepturi incidunt sequi quaerat asperiores?",
+    link: "https://www.omarcode.me/",
+  },
+  {
+    name: "project 2",
+    content:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque esse excepturi incidunt sequi quaerat asperiores?",
+    link: "https://www.omarcode.me/",
+  },
+];
+
+export { PROJECTS };
